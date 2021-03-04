b31555fcdfd1f583e5d9ce199d3211bb048db499
jtblane <63323583+jtblane@users.noreply.github.com>
2021-03-04 10:45:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,23 +37,22 @@ chart = alt.Chart(df).mark_point().encode(
 st.write(chart)
 
 
-'''code for a map
+# code for a map
 
-import altair as alt
-from vega_datasets import data
+# import altair as alt
+# from vega_datasets import data
 
-counties = alt.topo_feature(data.us_10m.url, 'counties')
-source = data.unemployment.url
+# counties = alt.topo_feature(data.us_10m.url, 'counties')
+# source = data.unemployment.url
 
-alt.Chart(counties).mark_geoshape().encode(
-    color='rate:Q'
-).transform_lookup(
-    lookup='id',
-    from_=alt.LookupData(source, 'id', ['rate'])
-).project(
-    type='albersUsa'
-).properties(
-    width=500,
-    height=300
-)
-'''
+# alt.Chart(counties).mark_geoshape().encode(
+#     color='rate:Q'
+# ).transform_lookup(
+#     lookup='id',
+#     from_=alt.LookupData(source, 'id', ['rate'])
+# ).project(
+#     type='albersUsa'
+# ).properties(
+#     width=500,
+#     height=300
+# )
