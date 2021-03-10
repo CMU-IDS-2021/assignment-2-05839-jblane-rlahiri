72dd071caa8c2603a54b59a6a35e630a9f902a16
Riddhiman Lahiri <78439141+Ridman1997@users.noreply.github.com>
2021-03-10 00:45:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ for i in range(county_data.shape[0]):
     county_details.update({str(covidcast.fips_to_name(county_data.iloc[i]))[2:len(str(covidcast.fips_to_name(county_data.iloc[i])))-2]:l[i]})
 
     
-input_drop=alt.binding_select(options=(list(county_details.values())),name="Select County by FIPS code")
+input_drop=alt.binding_select(options=(list(county_details.values())),name="Select County to Highlight data for Bar visits")
 picked=alt.selection_single(encodings=["color"],bind=input_drop) 
 scatter=alt.Chart(data_barvis_PA).mark_line().encode(
     x=alt.X("monthdate(time_value):O"),
@@ -86,8 +86,8 @@ l=county_data["geo_value"].tolist()
 print(str(covidcast.fips_to_name(county_data.iloc[1])))
 for i in range(county_data.shape[0]):
     county_details.update({str(covidcast.fips_to_name(county_data.iloc[i]))[2:len(str(covidcast.fips_to_name(county_data.iloc[i])))-2]:l[i]})
-    #county_details.update({l[i]:covidcast.fips_to_name(county_data.iloc[i])})
-input_drop=alt.binding_select(options=(list(county_details.values())),name="Select County by FIPS code")
+  
+input_drop=alt.binding_select(options=(list(county_details.values())),name="Select County to Highlight data for Restaurant visits")
 picked=alt.selection_single(encodings=["color"],bind=input_drop) 
 
 
@@ -95,18 +95,15 @@ bar_dataPA=data3_6hr = pandasql.sqldf("select * from data where geo_value like '
 
 scatter1=alt.Chart(bar_dataPA).mark_line(point=True).encode(
     x=alt.X("monthdate(time_value):O"),
-    #alt.Y("geo_value:O",bin=True,axis=alt.Axis(format='%', title='percentage')),
-    #x="time_value:O",
+    
     y=alt.Y("value:Q",axis=alt.Axis(title='Average number of daily bar visits')),
     tooltip=['geo_value','monthdate(time_value)','value'],
-    #alt.Color("geo_value")
-    #x="time_value:O",
-    #y="value:Q"
+    
     color=alt.condition(picked,'geo_value',alt.value('lightgray')),
     opacity=alt.condition(picked,alt.value(1),alt.value(0.05))
     
     ).add_selection(picked).properties(width=800,height=400,title="Percentage of people spending 3-6 hours outside").interactive()
-#scatter.properties(width=800,height=600,title="Percentage of people spending 3-6 hours outside")
+
 st.write(scatter1+scatter)
 
 
